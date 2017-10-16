54fe86383922c17128b13fcd76f3231879cefc6f
kelceydamage <kelcey@kelceydamage.com>
2017-10-16 18:44:54 -0400

    cleaned up test to run in multiprocessing

--- tasks/cuda_test.py
+++ tasks/cuda_test.py
@@ -23,63 +23,24 @@ SUMMARY:                Starter script for storing registered functions
 
 # Imports
 #-------------------------------------------------------------------------------- <-80
-import pycuda.driver as cuda
-import pycuda.autoinit
-from pycuda.compiler import SourceModule
-import numpy
-import time
-import math
+from __future__ import absolute_import
 
 # Globals
 #-------------------------------------------------------------------------------- <-80
-WIDTH = 32
-DEPTH = 32
-TOTAL = WIDTH * DEPTH
-BLOCK = 32
-GRID = int(math.ceil(math.sqrt((TOTAL / BLOCK)) / 8))
-GRID = GRID - (GRID % 32)
-if GRID == 0:
-        GRID = 1
 
 # Classes
 #-------------------------------------------------------------------------------- <-80
 
 # Functions
 #-------------------------------------------------------------------------------- <-80
-def main(func, a, a_doubled):
-    t0 = time.time()
-    t1 = time.time()
+def main(cuda, func, a, a_doubled):
     a_gpu = cuda.mem_alloc(a.nbytes)
-    mem_allocate = time.time() - t1
-    #print('Allocate to CUDA memory: {0}s'.format(mem_allocate))
-
-    t11 = time.time()
     cuda.memcpy_htod_async(a_gpu, a)
-    mem_cp_time = time.time() - t11
-    #print('Copy object to CUDA memory: {0}s'.format(mem_cp_time))
-
-    t = time.time()
-    func(a_gpu, block=(BLOCK, BLOCK, 1), grid=(100, 100))
-    total_time = time.time() - t
-    #print('Call CUDA Kernel: {0}'.format(total_time))
-
-    t3 = time.time()
+    func(a_gpu, block=(32, 32, 1), grid=(32, 32))
     cuda.memcpy_dtoh(a_doubled, a_gpu)
-    recv_mem = time.time() - t3
-    #print('Retrieve object from CUDA memory: {0}'.format(recv_mem))
+    return a_doubled
 
-    return total_time, recv_mem, mem_cp_time, t0
-    
-
-def print_out(total_time, recv_mem, mem_cp_time, t0):
-    t_time = total_time + recv_mem + mem_cp_time
-    print('--------------------------------------------------------')
-    print('Took: {0}s'.format(t_time))
-    print('Rate: {0:f} / s'.format(round(TOTAL / t_time, 2)))
-    print('End to end: {0}s'.format(time.time() - t0))
-    print('--------------------------------------------------------')
-
-def gen_kernel():
+def gen_kernel(SourceModule):
     return SourceModule("""
         __global__ void double_it(float *a)
         {
@@ -88,58 +49,39 @@ def gen_kernel():
         }
         """)
 
-def compile_kernel():
-    t2 = time.time()
-    mod = gen_kernel()
+def compile_kernel(SourceModule):
+    mod = gen_kernel(SourceModule)
     func = mod.get_function("double_it")
-    setup_kern = time.time() - t2
-    print('Compile CUDA Kernel: {0}'.format(setup_kern))
     return func
 
-def gen_objects():
-    t0 = time.time()
+def gen_objects(numpy, WIDTH, DEPTH):
     a = numpy.random.randn(WIDTH, DEPTH)
     a = a.astype(numpy.float32)
     a_doubled = numpy.empty_like(a)
-    init_time = time.time() - t0
-    print('Generate numpy object time: {0}'.format(init_time))
     return a, a_doubled
 
 def task_get_cuda_test(*args, **kwargs):
-    print('--------------------------------------------------------')
-    print('Doubling float matrix of {0} x {1} [{2}]'.format(WIDTH, DEPTH, TOTAL))
-    print('Grid: ({0},{0}), Block: ({1},{1},1)'.format(100, BLOCK))
-    a, a_doubled = gen_objects()
-    func  = compile_kernel()
+    import pycuda.driver as cuda
+    import pycuda.autoinit
+    from pycuda.compiler import SourceModule
+    import numpy
+    import time
+    a, a_doubled = gen_objects(numpy, 32, 32)
+    try:
+        func  = compile_kernel(SourceModule)
+    except Exception, e:
+        return str(e)
+    results = []
     for i in xrange(4):
-        total_time, recv_mem, mem_cp_time, t0 = main(func, a, a_doubled)
-        print_out(total_time, recv_mem, mem_cp_time, t0)
+        start = time.time()
+        try:
+            a_doubled = main(cuda, func, a, a_doubled)
+        except Exception, e:
+            return str(e)
+        else:
+            end = time.time() - start
+            results.append((len(a_doubled), end))
+    return results
 
 # Main
 #-------------------------------------------------------------------------------- <-80
-if __name__ == '__main__':
-    print('--------------------------------------------------------')
-    
-    
-    func = compile_kernel()
-    matrices = []
-    times = []
-    rates = []
-    for i in xrange(10):
-        print(WIDTH, DEPTH)
-        print('Doubling float matrix of {0} x {1} [{2}]'.format(WIDTH, DEPTH, TOTAL))
-        print('Grid: ({0},{0}), Block: ({1},{1},1)'.format(100, BLOCK))
-        a, a_doubled = gen_objects()
-        total_time, recv_mem, mem_cp_time, t0 = main(func, a, a_doubled)
-        t_time = total_time + recv_mem + mem_cp_time
-        print_out(total_time, recv_mem, mem_cp_time, t0)
-        matrices.append(str(TOTAL))
-        times.append(str(t_time))
-        rates.append(str(round(TOTAL / t_time, 2)))
-        WIDTH = WIDTH << 1
-        DEPTH = DEPTH << 1
-        TOTAL = WIDTH * DEPTH
-
-    print(','.join(matrices))
-    print(','.join(times))
-    print(','.join(rates))
