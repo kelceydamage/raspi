401bb6ca6a1a83976e084f6d0bafdbfb84be2ee8
kelcey-vagrant <kelcey@kelceydamage.com>
2018-08-05 21:10:59 -0700

    coverage updates

--- BUILD.sh
+++ BUILD.sh
@@ -7,11 +7,15 @@ fi
 
 # precompile c++ modules
 echo -e "PRE_COMPILING PXD\n"
-cython -a --cplus $PROJECT/drive.pyx --force --embed
+cython --annotate-coverage coverage.xml $PROJECT/drive.pyx --force --embed --cplus
 
 # general compile of project modules
 echo -e "RUNNING SETUP.PY\n"
 python3 $PROJECT/setup.py build_ext --inplace --force
+
+# postcompile c++ modules
+echo -e "POST_COMPILING PXD for annotations and coverage\n"
+cython --annotate-coverage coverage.xml $PROJECT/drive.pyx --force --embed --cplus
 
 echo -e "DONE\n"
 # distribute and cleanup
