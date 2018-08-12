090d21c9eff5ac2def3ba45f9ef3cd5e8b9420bf
kelcey-vagrant <kelcey@kelceydamage.com>
2018-08-11 20:49:19 -0700

    converted datatypes to C++ struct wrappers. Added API for working with python

--- common/datatypes.pxd
+++ common/datatypes.pxd
@@ -1,20 +1,152 @@
+#!python
+#cython: language_level=3, cdivision=True
+###boundscheck=False, wraparound=False //(Disabled by default)
+# ------------------------------------------------------------------------ 79->
+# Author: Kelcey Damage
+# Cython: 0.28+
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Doc
+# ------------------------------------------------------------------------ 79->
+
+# Imports
+# ------------------------------------------------------------------------ 79->
+cimport cython
+from libcpp.list cimport list as cpplist
+from libcpp cimport bool
+from libcpp.vector cimport vector
+from libcpp.utility cimport pair
+from libcpp.string cimport string
+from libcpp.map cimport map
+from libcpp.unordered_map cimport unordered_map
+from libc.stdint cimport uint_fast32_t
+from libc.stdint cimport uint_fast16_t
+from libc.stdint cimport int_fast16_t
+from libc.stdio cimport printf
+from libc.stdlib cimport atoi
+from posix cimport time as p_time
+
+# Globals
+# ------------------------------------------------------------------------ 79->
+cdef public struct MetaMessage:
+    string id
+    string role
+    string version
+    string type
+    string pack
+    string serial
+    uint_fast16_t part
+    uint_fast16_t length
+    uint_fast16_t size
+    string error
+
+cdef struct TaskMessage:
+    string task
+    vector[string] args
+    vector[double] nargs
+    map[string, string] kwargs
+    string pack
+    string error
+
+cdef struct DataMessage:
+    vector[string] data
+    string pack
+    string error
+    uint_fast16_t size
+
+cdef MetaMessage M_MESSAGE
+cdef TaskMessage T_MESSAGE
+cdef DataMessage D_MESSAGE
+
+# Classes
+# ------------------------------------------------------------------------ 79->
 cdef class Frame:
-    cdef public int pack
+    cdef public uint_fast32_t pack
     cdef public dict message
-    cdef public object hash
-        
-    cpdef serialize(self)
-    cpdef pack_frame(Frame self, dict kwargs)
-    cpdef digest(Frame self)
+    cdef public string hash
+
+    cpdef string serialize(Frame self)
+    cpdef Frame deserialize(Frame self, string message)
+    cpdef void _pack_frame(Frame self, dict kwargs)
+    cpdef void digest(Frame self)
 
 cdef class MetaFrame(Frame):
-
-    cpdef gen_message(MetaFrame self)
+    cdef MetaMessage* _message
+    
+    # Python safe API. Use this to interact with C++ pointer structs
+    cpdef MetaMessage get_message(MetaFrame self)
+    cpdef void set_pack(MetaFrame self, string pack)
+    cpdef string get_pack(MetaFrame self)
+    cpdef void set_error(MetaFrame self, string error)
+    cpdef string get_error(MetaFrame self)
+    cpdef void set_id(MetaFrame self, string id)
+    cpdef string get_id(MetaFrame self)
+    cpdef void set_role(MetaFrame self, string role)
+    cpdef string get_role(MetaFrame self)
+    cpdef void set_version(MetaFrame self, string version)
+    cpdef string get_version(MetaFrame self)
+    cpdef void set_type(MetaFrame self, string _type)
+    cpdef string get_type(MetaFrame self)
+    cpdef void set_serial(MetaFrame self, string serial)
+    cpdef string get_serial(MetaFrame self)
+    cpdef void set_part(MetaFrame self, uint_fast16_t part)
+    cpdef uint_fast16_t get_part(MetaFrame self)
+    cpdef void set_length(MetaFrame self, uint_fast16_t length)
+    cpdef uint_fast16_t get_length(MetaFrame self)
+    cpdef void gen_message(MetaFrame self, dict params)
+    cpdef void set_size(MetaFrame self, uint_fast16_t size)
+    cpdef uint_fast16_t get_size(MetaFrame self)
 
 cdef class DataFrame(Frame):
+    cdef DataMessage* _message
 
-    cpdef gen_message(self)
+    # Python safe API. Use this to interact with C++ pointer structs
+    cpdef DataMessage get_message(DataFrame self)
+    cpdef void set_pack(DataFrame self, string pack)
+    cpdef string get_pack(DataFrame self)
+    cpdef void set_error(DataFrame self, string error)
+    cpdef string get_error(DataFrame self)
+    cpdef void gen_message(DataFrame self, dict params)
+    cpdef void set_size(DataFrame self, uint_fast16_t size)
+    cpdef uint_fast16_t get_size(DataFrame self)
+    cpdef void set_data(DataFrame self, vector[string] data)
+    cpdef vector[string] get_data(DataFrame self)
 
 cdef class TaskFrame(Frame):
+    cdef TaskMessage* _message
 
-    cpdef gen_message(self)
+    # Python safe API. Use this to interact with C++ pointer structs
+    cpdef TaskMessage get_message(TaskFrame self)
+    cpdef void set_pack(TaskFrame self, string pack)
+    cpdef string get_pack(TaskFrame self)
+    cpdef void set_error(TaskFrame self, string error)
+    cpdef string get_error(TaskFrame self)
+    cpdef void set_task(TaskFrame self, string task)
+    cpdef string get_task(TaskFrame self)
+    cpdef void set_args(TaskFrame self, vector[string] args)
+    cpdef vector[string] get_args(TaskFrame self)
+    cpdef void set_kwargs(TaskFrame self, map[string, string] kwargs)
+    cpdef map[string, string] get_kwargs(TaskFrame self)
+    cpdef void gen_message(TaskFrame self, dict params)
+    cpdef void set_nargs(TaskFrame self, vector[double] nwargs)
+    cpdef vector[double] get_nargs(TaskFrame self)
+
+# Functions
+# ------------------------------------------------------------------------ 79->
+
+# Main
+# ------------------------------------------------------------------------ 79->
+if __name__ == '__main__':
+    pass
--- common/datatypes.pyx
+++ common/datatypes.pyx
@@ -25,6 +25,19 @@ SUMMARY: Classes to represent various message frame datatypes
 #-------------------------------------------------------------------------------- <-80
 import json
 import hashlib
+cimport cython
+from libcpp.list cimport list as cpplist
+from libcpp cimport bool
+from libcpp.vector cimport vector
+from libcpp.utility cimport pair
+from libcpp.string cimport string
+from libcpp.map cimport map
+from libcpp.unordered_map cimport unordered_map
+from libc.stdint cimport uint_fast32_t
+from libc.stdint cimport int_fast16_t
+from libc.stdio cimport printf
+from libc.stdlib cimport atoi
+from posix cimport time as p_time
 
 # Globals
 #-------------------------------------------------------------------------------- <-80
@@ -36,34 +49,43 @@ cdef class Frame:
 NAME:
 DESCRIPTION:
     """
-    def __cinit__(Frame self, int pack):
+    def __cinit__(Frame self, uint_fast32_t pack):
         self.pack = pack
         
-    cpdef serialize(self):
+    cpdef string serialize(Frame self):
         """
 NAME:           serialize
 DESCRIPTION:    Convert self.message into a json object
         """
-        return json.dumps(self.message).encode()
+        return str(self.get_message()).encode()
 
-    cpdef pack_frame(Frame self, dict kwargs):
+    cpdef Frame deserialize(Frame self, string message):
         """
-NAME:           pack_frame
-DESCRIPTION:    Helper to setup a frame
+NAME:           serialize
+DESCRIPTION:    Convert self.message into a json object
+        """
+        return dict(self.message.decode())
+
+    cpdef void _pack_frame(Frame self, dict kwargs):
+        """
+NAME:           pack_frames
+DESCRIPTION:    Helper to setup an arbitrary frame
         """
         for key in kwargs:
             setattr(self, key, kwargs[key])
 
-    cpdef digest(Frame self):
-        self.hash = hashlib.md5(''.join(sorted(self.message)).encode()).hexdigest()
+    cpdef void digest(Frame self):
+        self.hash = hashlib.md5(''.join(sorted(self.get_message())).encode()).hexdigest().encode()
 
 cdef class MetaFrame(Frame):
     """
 NAME:           MetaFrame
 DESCRIPTION:    Frame object for metadata
     """
-    def __cinit__(MetaFrame self, int pack):
+    def __cinit__(MetaFrame self, uint_fast32_t pack):
         super(MetaFrame, self).__init__(pack)
+        self._message = <MetaMessage*>&M_MESSAGE
+        self._message.part = <uint_fast16_t>0
         self.message = {
             'id': '',
             'role': '',
@@ -76,7 +98,71 @@ DESCRIPTION:    Frame object for metadata
         }
         self.digest()
 
-    cpdef gen_message(MetaFrame self):
+    cpdef MetaMessage get_message(MetaFrame self):
+        # return message safely convertable to a python dict
+        return <MetaMessage>self._message[0]
+
+    cpdef void set_pack(MetaFrame self, string pack):
+        self._message.pack = pack
+
+    cpdef string get_pack(MetaFrame self):
+        return self._message.pack
+
+    cpdef void set_error(MetaFrame self, string error):
+        self._message.error = error
+
+    cpdef string get_error(MetaFrame self):
+        return self._message.error
+
+    cpdef void set_id(MetaFrame self, string id):
+        self._message.id = id
+
+    cpdef string get_id(MetaFrame self):
+        return self._message.id
+
+    cpdef void set_role(MetaFrame self, string role):
+        self._message.role = role
+
+    cpdef string get_role(MetaFrame self):
+        return self._message.role
+
+    cpdef void set_version(MetaFrame self, string version):
+        self._message.version = version
+
+    cpdef string get_version(MetaFrame self):
+        return self._message.version
+
+    cpdef void set_type(MetaFrame self, string _type):
+        self._message.type = _type
+
+    cpdef string get_type(MetaFrame self):
+        return self._message.type
+
+    cpdef void set_serial(MetaFrame self, string serial):
+        self._message.serial = serial
+
+    cpdef string get_serial(MetaFrame self):
+        return self._message.serial
+
+    cpdef void set_part(MetaFrame self, uint_fast16_t part):
+        self._message.part = part
+    
+    cpdef uint_fast16_t get_part(MetaFrame self):
+        return self._message.part
+
+    cpdef void set_length(MetaFrame self, uint_fast16_t length):
+        self._message.length = length
+
+    cpdef uint_fast16_t get_length(MetaFrame self):
+        return self._message.length
+
+    cpdef void set_size(MetaFrame self, uint_fast16_t size):
+        self._message.size = size
+
+    cpdef uint_fast16_t get_size(MetaFrame self):
+        return self._message.size
+
+    cpdef void gen_message(MetaFrame self, dict params):
         """
 NAME:           gen_message
 DESCRIPTION:    Converts object into a serializable dict
@@ -87,28 +173,60 @@ REQUIRES:       self.id [formatted as '{ROLE}-{PID}'] Example: 'W-3223'
                 self.pack [Package ID]
         """
         try:
-            self.message['id'] = self.id
-            self.message['role'] = self.role
-            self.message['version'] = self.version
-            self.message['type'] = self.type
-            self.message['pack'] = self.pack
+            self._message.id = <string>params['id']
+            self._message.role = <string>params['role']
+            self._message.version = <string>params['version']
+            self._message.type = <string>params['type']
+            self._message.pack = <string>params['pack']
+            self._message.serial = <string>params['serial']
+            self._message.part = <uint_fast16_t>params['part']
+            self._message.length = <uint_fast16_t>params['length']
         except Exception as e:
-            self.message['error'] = b'Missing key parameter'
+            self._message.error = b'Missing key parameter'
 
 cdef class DataFrame(Frame):
     """
 NAME:           DataFrame
 DESCRIPTION:    Frame object for data
     """
-    def __cinit__(DataFrame self, int pack):
+    def __cinit__(DataFrame self, uint_fast32_t pack):
         super(DataFrame, self).__init__(pack)
+        self._message = <DataMessage*>&D_MESSAGE
         self.message = {
             'data': '',
             'pack': ''
         }   
         self.digest()
 
-    cpdef gen_message(DataFrame self):
+    cpdef DataMessage get_message(DataFrame self):
+        # return message safely convertable to a python dict
+        return <DataMessage>self._message[0]
+
+    cpdef void set_pack(DataFrame self, string pack):
+        self._message.pack = pack
+
+    cpdef string get_pack(DataFrame self):
+        return self._message.pack
+
+    cpdef void set_error(DataFrame self, string error):
+        self._message.error = error
+
+    cpdef string get_error(DataFrame self):
+        return self._message.error
+
+    cpdef void set_size(DataFrame self, uint_fast16_t size):
+        self._message.size = size
+
+    cpdef uint_fast16_t get_size(DataFrame self):
+        return self._message.size
+
+    cpdef void set_data(DataFrame self, vector[string] data):
+        self._message.data = data
+
+    cpdef vector[string] get_data(DataFrame self):
+        return self._message.data
+
+    cpdef void gen_message(DataFrame self, dict params):
         """
 NAME:           gen_message
 DESCRIPTION:    Converts object into a serializable dict
@@ -116,18 +234,20 @@ REQUIRES:       self.data [Data payload]
                 self.pack [Package ID]
         """
         try:
-            self.message['data'] = self.data
-            self.message['pack'] = self.paack
+            self._message.pack = <string>params['pack']
+            self._message.data = <vector[string]>params['data']
+            self._message.size = <uint_fast16_t>params['size']
         except Exception as e:
-            self.message['error'] = b'Missing key parameter'
+            self._message.error = b'Missing key parameter'
 
 cdef class TaskFrame(Frame):
     """
 NAME:           TaskFrame
 DESCRIPTION:    Frame object for tasks
     """
-    def __cinit__(TaskFrame self, int pack):
+    def __cinit__(TaskFrame self, uint_fast32_t pack):
         super(TaskFrame, self).__init__(pack)
+        self._message = <TaskMessage*>&T_MESSAGE
         self.message = {
             'task': '',
             'args': '',
@@ -136,7 +256,47 @@ DESCRIPTION:    Frame object for tasks
         }  
         self.digest() 
 
-    cpdef gen_message(TaskFrame self):
+    cpdef TaskMessage get_message(TaskFrame self):
+        # return message safely convertable to a python dict
+        return <TaskMessage>self._message[0]
+
+    cpdef void set_pack(TaskFrame self, string pack):
+        self._message.pack = pack
+
+    cpdef string get_pack(TaskFrame self):
+        return self._message.pack
+
+    cpdef void set_error(TaskFrame self, string error):
+        self._message.error = error
+
+    cpdef string get_error(TaskFrame self):
+        return self._message.error
+
+    cpdef void set_task(TaskFrame self, string task):
+        self._message.task = task
+
+    cpdef string get_task(TaskFrame self):
+        return self._message.task
+
+    cpdef void set_args(TaskFrame self, vector[string] args):
+        self._message.args = args
+
+    cpdef vector[string] get_args(TaskFrame self):
+        return self._message.args
+
+    cpdef void set_kwargs(TaskFrame self, map[string, string] kwargs):
+        self._message.kwargs = kwargs
+
+    cpdef map[string, string] get_kwargs(TaskFrame self):
+        return self._message.kwargs
+
+    cpdef void set_nargs(TaskFrame self, vector[double] nargs):
+        self._message.nargs = nargs
+
+    cpdef vector[double] get_nargs(TaskFrame self):
+        return self._message.nargs
+
+    cpdef void gen_message(TaskFrame self, dict params):
         """
 NAME:           gen_message
 DESCRIPTION:    Converts object into a serializable dict
@@ -146,12 +306,13 @@ REQUIRES:       self.task [Name of task to run]
                 self.pack [Package ID]
         """
         try:
-            self.message['task'] = self.task
-            self.message['args'] = self.args
-            self.message['kwargs'] = self.kwargs
-            self.message['pack'] = self.pack 
+            self._message.task = <string>params['task']
+            self._message.args = <vector[string]>params['args']
+            self._message.nargs = <vector[double]>params['nargs']
+            self._message.kwargs = <map[string, string]>params['kwargs']
+            self._message.pack = <string>params['pack']
         except Exception as e:
-            self.message['error'] = b'Missing key parameter'
+            self._message.error = b'Missing key parameter'
 
 # Functions
 #-------------------------------------------------------------------------------- <-80
